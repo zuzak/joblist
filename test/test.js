d684d10aca253dbd373fde29dc2dbdb6851d6718
Douglas Gardner <douglas@chippy.ch>
2016-08-23 22:19:56 +0100

    Add simple body-parsing tests

--- test/test.js
+++ test/test.js
@@ -1,16 +1,29 @@
 var request = require( 'supertest' ); // bubbling blancmange
+var should = require( 'should' );
 var app = require( '..' );
 
 describe( 'running Express.js', function () {
-	it( 'responds with a homepage', function ( done ) {
-		request( app )
-			.get( '/' )
-			.expect( 200, done );
-	} );
 	it( 'responds with a 404', function ( done ) {
 		request( app )
 			.get( '/foo' )
 			.expect( 404, done );
+	} );
+} );
+
+describe( 'index page', function () {
+	it( 'should return something', function ( done ) {
+		request( app )
+			.get( '/' )
+			.expect( 200, done );
+	} );
+	it ( 'should return a list of applicants', function ( done ) {
+		request( app )
+			.get( '/' )
+			.end( function ( err, res ) {
+				res.text.should.containEql( 'Gaston LEROUX' );
+				res.text.should.containEql( 'Lewis CAROLL' );
+				done();
+			} );
 	} );
 } );
 
@@ -20,10 +33,28 @@ describe( 'applicant listings', function () {
 			.get( '/applicant/0' )
 			.expect( 200, done );
 	} );
+	it( 'displays the applicant name somewhere on the listing', function ( done ) {
+		request( app )
+			.get( '/applicant/0' )
+			.expect( 200 )
+			.end( function ( err, res ) {
+				res.text.should.containEql( 'Gaston LEROUX' );
+				done();
+			} );
+	} );
 	it( 'responds with a 404 with a non-existent applicant', function ( done ) {
 		request( app )
 			.get( '/applicants/-1' )
 			.expect( 404, done );
+	} );
+	it( 'displays the applicant experience on the listing', function ( done ) {
+		request( app )
+			.get( '/applicant/0' )
+			.expect( 200 )
+			.end( function ( err, res ) {
+				res.text.should.containEql( 'Christine Daae' );
+				done();
+			} );
 	} );
 } );
 
